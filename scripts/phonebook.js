02d9dfd0774b62d4bb1c53b7b9b5324963b5d625
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-06-13 11:55:49 -0400

    Update phonebook.js

--- scripts/phonebook.js
+++ scripts/phonebook.js
@@ -89,7 +89,7 @@ window.onload = () => {
             tdAdd.textContent = cursor.value.address
             tdTel.textContent = cursor.value.telephone
             tdEmail.textContent = cursor.value.email
-            tdUrl.textContent = "https://google.com"
+            tdUrl.textContent = <a href="https://cisco.com">Google</a>
     
             tr.setAttribute('data-contact-id', cursor.value.id);
     
